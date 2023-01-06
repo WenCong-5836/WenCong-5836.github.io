e53ac2a70eab0333b1cd03cd4415810a549822b9
Hiysters <2216328098@qq.com>
2023-01-06 10:29:51 +0800

    Site updated: 2023-01-06 10:29:50

--- css/injector.js
+++
@@ -1,9 +0,0 @@
-// ȫ����������Ҫ������Щjs
-const { root: siteRoot = "/" } = hexo.config;
-hexo.extend.injector.register("body_begin", `<div id="web_bg"></div>`);
-hexo.extend.injector.register(
-  "body_end",
-  `<script src="${siteRoot}js/backgroundize.js"></script>
-  <link defer rel="stylesheet" href="${siteRoot}styles/backgroundize.css" />
-  `
-);
